c0497bfa5e26efea1651ca7099b4921d2eaaa548
LarissaCns <larissacristina.nunes@outlook.com>
2022-10-19 23:20:19 -0300

    feat: :sparkles: Função carrega_palavra_secreta
    Criação da função responsável por ler um arquivo de texto e armazenar as palavras contidas nele em um array

--- jogo.py
+++ jogo.py
@@ -1,4 +1,21 @@
+import random 
+
 def imprime_mensagem_abertura():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
+
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+    return palavra_secreta

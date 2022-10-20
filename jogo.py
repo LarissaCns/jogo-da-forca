469f7c55645f57859864f624be8130af1454c1e7
LarissaCns <larissacristina.nunes@outlook.com>
2022-10-19 23:24:46 -0300

    feat: :sparkles: Função marca_chute_correto
    Criação da função responsável por colocar o chute do usuário na posição correta, dentro da lista de 'riscos'

--- jogo.py
+++ jogo.py
@@ -31,3 +31,11 @@ def pede_chute():
     chute = chute.strip().upper()
 
     return chute
+
+
+def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if (chute == letra):
+            letras_acertadas[index] = letra
+        index += 1
